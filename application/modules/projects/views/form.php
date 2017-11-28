c9ae87b31056567e837f0db606e7b52dab55f855
Gilbert Brault <gilbert.brault@hotmail.com>
2017-11-28 14:29:07 +0100

    added populate Client dropdown
    see https://community.invoiceplane.com/t/topic/5310

--- application/modules/projects/views/form.php
+++ application/modules/projects/views/form.php
@@ -25,7 +25,14 @@
         </div>
         <div class="form-group">
             <label for="client_id"><?php _trans('client'); ?></label>
-            <select name="client_id" id="client_id" class="form-control" autofocus="autofocus"></select>
+            <select name="client_id" id="client_id" class="form-control" autofocus="autofocus">
+                  <?php
+                    foreach ($clients as $client) {
+                        echo "<option value=\"" . $client->client_id . "\" ";
+                        echo ">" . htmlsc(format_client($client)) . "</option>";
+                    }
+                  ?>
+            </select>
         </div>
 
     </div>
